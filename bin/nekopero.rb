10300de703ee965909e39333e44a0c647d6cc5d4
KUMAZAKI Hiroki <kumazaki.hiroki@lab.ntt.co.jp>
2012-10-05 10:08:50 +0900

    move to lib

--- bin/nekopero.rb
+++ bin/nekopero.rb
@@ -1,17 +1,12 @@
 require "json"
 
 module Nekopero
-  def setting_file_candidate dirname
-    Dir::entries(dirname).map{|x| x.gsub(/.yaml$/,"")}.map{|x| x.gsub(/.json$/,"")}.reject{|x| x.include?(".") || x.include?("~")}
-  end
-
   # TODO: set by argument
   host = "localhost"
   port = 9199
-
   case ARGV[0]
   when "classifier", "c"
-    require "./classifier"
+    require "../lib/classifier"
     jubaclassifier host, port, ARGV[1..-1]
   when "recommender", "rec"
     require "./recommender"
@@ -22,5 +17,7 @@ module Nekopero
     require "jubatus/stat/client"
   when "graph", "g"
     require "jubatus/graph/client"
+  else
+    puts "you can select algorithm within classifier, recommender, regression, stat, graph"
   end
 end
--- bin/utils.rb
+++
@@ -1,3 +0,0 @@
-def setting_file_candidate dirname
-  Dir::entries(dirname).map{|x| x.gsub(/.yaml$/,"")}.map{|x| x.gsub(/.json$/,"")}.reject{|x| x.include?(".") || x.include?("~")}
-end
